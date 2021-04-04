881888499b7104dff0700a8ac9fda15b333b885a
Ryan <ryanlepham9@gmail.com>
2021-04-04 11:04:48 -0400

    added helper function equalsIgnoreCase() to compare two strings case insensitive

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function computerPlay()
 {
-    let computerInput = (Math.round(Math.random() * 3));
+    computerInput = (Math.round(Math.random() * 3));
     if (computerInput === 1)
     {
         return "Rock";
@@ -14,3 +14,19 @@ function computerPlay()
         return "Scissors";
     }
 }
+
+//Helper function to compare two strings that are case insensitive
+function equalsIgnoreCase(a, b)
+{
+    /*  localeCompare() returns -1 if a is sorted before b (ab and cd), 0 if equal, 1 if a is sorted after b (cd and ab)
+        { sensitivity: "accent" } treats two variables of same base letter equal unless different accents
+        { sensitivity: "base" }   treats two characters of same base letter equal */
+    return typeof a === "string" && typeof b === "string"
+        ? a.localeCompare(b, undefined, { sensitivity: "accent" }) === 0  
+        : a === b;
+}
+
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
