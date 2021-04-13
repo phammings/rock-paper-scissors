405da859b6a437333d0fc403818727f042db3f7a
Ryan <ryanlepham9@gmail.com>
2021-04-12 22:51:14 -0400

    added images to index.html, updated computerPlay(), removed isValidPlay() and checkScore(), game(), edited playRound to console.log roundStatements, and added eventListeners on click for buttons

--- script.js
+++ script.js
@@ -1,121 +1,93 @@
+const selection = ["rock", "paper", "scissors"];
+
 //Helper function for computer to randomly generate a play
-function computerPlay()
-{
-    computerInput = (Math.round(Math.random() * 3));
-    if (computerInput === 1)
-    {
-        return "Rock";
-    }
-    else if (computerInput === 2)
-    {
-        return "Paper";
-    }
-    else 
-    {
-        return "Scissors";
-    }
+function computerPlay() {
+  return selection[Math.round(Math.random() * (selection.length - 1)) + 1];
 }
 
 //Helper function to compare two strings that are case insensitive
-function equalsIgnoreCase(a, b)
-{
-    /*  localeCompare() returns -1 if a is sorted before b (ab and cd), 0 if equal, 1 if a is sorted after b (cd and ab)
+function equalsIgnoreCase(a, b) {
+  /*  localeCompare() returns -1 if a is sorted before b (ab and cd), 0 if equal, 1 if a is sorted after b (cd and ab)
         { sensitivity: "accent" } treats two variables of same base letter equal unless different accents
         { sensitivity: "base" }   treats two characters of same base letter equal */
-    return typeof a === "string" && typeof b === "string"
-        ? a.localeCompare(b, undefined, { sensitivity: "accent" }) === 0  
-        : a === b;
-}
-
-//Helper function to check if user input is a valid play
-function isValidPlay(playerSelection)
-{
-    return equalsIgnoreCase(playerSelection, "Rock") || 
-            equalsIgnoreCase(playerSelection, "Paper") || 
-            equalsIgnoreCase(playerSelection, "Scissors");
+  return typeof a === "string" && typeof b === "string"
+    ? a.localeCompare(b, undefined, { sensitivity: "accent" }) === 0
+    : a === b;
 }
 
 //Plays 1 round of rock, paper, scissors
-function playRound(playerSelection, computerSelection)
-{
-    playerSelection = prompt("Enter your play");
-    computerSelection = computerPlay();
-    if (!(isValidPlay(playerSelection)))
-    {
-        console.log("Invalid Play. Choose either rock, paper, or scissors.");
-        playRound(playerSelection, computerPlay());
-    }
-    else if (equalsIgnoreCase(playerSelection, computerSelection))
-    {
-        console.log(`It's a draw! ${playerSelection} ties with ${computerSelection}, try again!`);
-        playRound(playerSelection, computerPlay());
-    }
-    else if (equalsIgnoreCase(playerSelection, "Paper") && equalsIgnoreCase(computerSelection, "Rock") ||
-        equalsIgnoreCase(playerSelection, "Rock") && equalsIgnoreCase(computerSelection, "Scissors") ||
-        equalsIgnoreCase(playerSelection, "Scissors") && equalsIgnoreCase(computerSelection, "Paper"))
-    {
-        let roundStatement = console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
-        playerScore++;
-        return roundStatement;
-    }
-    else
-    {
-        let roundStatement = console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
-        computerScore++;
-        return roundStatement;
-    }
-    
+function playRound(playerSelection, computerSelection) {
+  computerSelection = computerPlay();
+  if (equalsIgnoreCase(playerSelection, computerSelection)) {
+    console.log(
+      `It's a draw! ${playerSelection} ties with ${computerSelection}, try again!`
+    );
+  } else if (
+    (equalsIgnoreCase(playerSelection, "Paper") &&
+      equalsIgnoreCase(computerSelection, "Rock")) ||
+    (equalsIgnoreCase(playerSelection, "Rock") && //make into a function
+      equalsIgnoreCase(computerSelection, "Scissors")) ||
+    (equalsIgnoreCase(playerSelection, "Scissors") &&
+      equalsIgnoreCase(computerSelection, "Paper"))
+  ) {
+    playerScore++;
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+  } else {
+    computerScore++;
+    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+  }
 }
 
 //Checks final score to determine winner
-function checkScore(playerScore, computerScore)
-{
-    if (playerScore>computerScore)
-    {
-        return `Congratulations! You won by ${ playerScore - computerScore } points!`
-    }
-    else{
-        return `You lost by ${ computerScore - playerScore } points!`
-    }
+function declareWinner(playerScore, computerScore) {
+  console.log(`Score:
+    Player:     ${playerScore}
+    Computer    ${computerScore}`);
+  if (playerScore > computerScore) {
+    console.log(
+      `Congratulations! You won by ${playerScore - computerScore} points!`
+    );
+  } else {
+    console.log(`You lost by ${computerScore - playerScore} points!`);
+  }
+  playAgain();
 }
 
 //Reset game scores and selections
-function resetGame()
-{
-    playerScore = 0;
-    computerScore = 0;
-    playerSelection = "";
-    computerSelection= "";
-    return;
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  playerSelection = "";
+  computerSelection = "";
+  location.reload();
 }
 
 //Prompts user to play again
-function playAgain()
-{
-    let ans = prompt("Play again? (Y/N)");
-    return equalsIgnoreCase(ans, "Y") ? game() : console.log("Thanks for playing!");
+function playAgain() {
+  let ans = prompt("Play again? (Y/N)");
+  return equalsIgnoreCase(ans, "Y")
+    ? resetGame()
+    : console.log("Thanks for playing!");
 }
 
-//Plays a full game of 5 rounds of rock, paper, scissors with a score 
-function game()
-{
-    console.log("Rock Paper Scissors (5 Rounds):")
-    for (var i=1; i<=5; i++)
-    {
-        console.log(`Round ${i}:`);
-        playRound(playerSelection, computerSelection);
-    }
-    console.log(`Score:
-                Player:     ${ playerScore }
-                Computer    ${ computerScore }`
-                );
-    console.log(checkScore(playerScore, computerScore));
-    playAgain();
-    resetGame();
-}
+//window.onload = intro();
 
 //Global variables
 let playerScore = 0;
 let computerScore = 0;
 let playerSelection = "";
-let computerSelection= "";
+let computerSelection = "";
+
+const button = document.querySelectorAll(".button");
+button.forEach((button) => {
+  button.addEventListener("click", () => {
+    const img = button.querySelector("img");
+    playerSelection = img.alt.toLowerCase();
+
+    playRound(playerSelection, computerSelection);
+    if (playerScore === 5 || computerScore === 5) {
+      declareWinner(playerScore, computerScore);
+    }
+  });
+});
+//add div to display results and change the console.log into DOM methods
